55d2eb9f2e73a179563a5b8f37c3c2200517cd48
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-03 21:37:35 +0530

    Connected router with server

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const projectsRouter = require('../routers/projects/projects-router');
+const actionsRouter = require('../routers/actions/actions-router');
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(helmet());
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json("Hi")
